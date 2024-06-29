f85208acecd91c8240faa3081f6ee17ffeb9bb25
Rehan Abbas <abbasrehan054@gmail.com>
2024-06-29 15:05:14 +0530

    Added all the dependencies

--- src/index.js
+++ src/index.js
@@ -6,8 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
@@ -15,8 +14,5 @@ root.render(
       </Provider>
     </BrowserRouter>
   </React.StrictMode>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
